cc35ad81ac39a0928aa6acd557b74a3aa5024587
+ <+>
2025-05-25 13:51:58 +0300

    emailperser

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+       
+        emails = re.findall(r'[\w\.-]+@[\w\.-]+\.\w+', self.email_string)
+
+        return sorted(set(emails))
